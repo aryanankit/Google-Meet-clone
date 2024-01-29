b8b237b4f7fb3ff4ca7fa9c653a72b7e15553dd1
aryanankit <a.arya8279@gmail.com>
2024-01-29 23:32:51 +0530

    Built the connectionform

--- src/components/ConnectionForm.tsx
+++ src/components/ConnectionForm.tsx
@@ -1,31 +1,48 @@
-import { useState } from "react";
-import logo from '../../assets/react.svg';
+import { useState } from 'react'
+import logo from './../assets/react.svg'
 
 interface ConnectFormProps {
-  connectToVideo: (channelName :  string ) => void
+  connectToVideo: (channelName: string) => void
 }
 
-export const ConnectForm = ( {} ) : ConnectFormProps) => {
-  const [channelName, setChannelName] = useState('');
-  const [invalidInputMsg, setInvalidInputMsg]  = useState('');
+export const ConnectForm = ({ connectToVideo } : ConnectFormProps) => {
 
+  const [channelName, setChannelName] = useState('')
+  const [invalidInputMsg, setInvalidInputMsg] = useState('')
+
+
+  const handleConnect = (e: React.FormEvent<HTMLFormElement>) => {
+    // trim spaces
+    const trimmedChannelName = channelName.trim()
+    
+    // validate input: make sure channelName is not empty
+    if (trimmedChannelName === '') {
+      e.preventDefault() // keep the page from reloading on form submission
+      setInvalidInputMsg("Channel name can't be empty.") // show warning
+      setChannelName('') // resets channel name value in case user entered blank spaces 
+      return;
+    } 
+  
+    connectToVideo(trimmedChannelName)
+  }
 
   return (
-    <form>
+    <form onSubmit={handleConnect}>
       <img src={logo} className="logo" alt="logo" />
       <div className="card">
-        <input
+        <input 
           id="channelName"
           type='text'
-          placeholder="channel Name"
+          placeholder='Channel Name'
           value={channelName}
-          onChange = {(e) => {
-            setChannelName(e.target.value);
+          onChange={(e) => {
+            setChannelName(e.target.value)
+            setInvalidInputMsg('') // clear the error message
           }}
         />
-
-        <button> Connect </button>
+        <button>Connect</button>
+        { invalidInputMsg && <p style={{color: 'red'}}> {invalidInputMsg} </p>}
       </div>
     </form>
-  );
+  )
 }
